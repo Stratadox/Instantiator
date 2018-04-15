246b96f27c3bebba3d8c51da9c1e8aea96c0946c
Stratadox <info@stratadox.com>
2018-04-16 00:06:23 +0200

    Add test for when predefined objects ran out

--- test/PredefinedObjects_iterates_over_a_list_of_instances.php
+++ test/PredefinedObjects_iterates_over_a_list_of_instances.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 namespace Stratadox\Instantiator\Test;
 
 use PHPUnit\Framework\TestCase;
+use Stratadox\Instantiator\CannotInstantiateThis;
 use Stratadox\Instantiator\PredefinedObjects;
 use Stratadox\Instantiator\Test\Fixtures\Bar;
 use Stratadox\Instantiator\Test\Fixtures\Foo;
 
 /**
  * @covers \Stratadox\Instantiator\PredefinedObjects
+ * @covers \Stratadox\Instantiator\OutOfObjects
  */
 class PredefinedObjects_iterates_over_a_list_of_instances extends TestCase
 {
@@ -37,5 +39,24 @@ class PredefinedObjects_iterates_over_a_list_of_instances extends TestCase
 
         $this->assertSame(Bar::class, $instances->class());
         $this->assertInstanceOf(Bar::class, $instances->instance());
+    }
+
+    /** @test */
+    function throwing_an_exception_when_out_of_objects()
+    {
+        $foo = new Foo;
+        $bar = new Bar;
+
+        $instances = PredefinedObjects::use($foo, $bar);
+
+        $instances->instance();
+        $instances->instance();
+
+        $this->expectException(CannotInstantiateThis::class);
+        $this->expectExceptionMessage(
+            'Out of objects!'
+        );
+
+        $instances->instance();
     }
 }
